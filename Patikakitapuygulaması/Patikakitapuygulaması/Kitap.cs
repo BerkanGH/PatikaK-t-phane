ee7a8f19cd66d3f559b48d2e7c8346cc9395a790
BerkanGH <berkankeskin28@gmail.com>
2024-10-22 17:25:48 +0300

    odev

---
+++ Patikakitapuygulaması/Patikakitapuygulaması/Kitap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patikakitapuygulaması
+{
+     class Kitap
+    {
+        // kitabın özellikleri ile ilgili bilgiler bunlar.
+        public string kitapAdı;
+        public string yazarAdı;
+        public string yazarSoyadı;
+        public int sayfaSayısı;
+        public string basımEvi;
+        public DateTime kayıtTarihi;
+
+        // bu default olan. Sadece kayıt tarihi o an girilcek.
+        public Kitap() { 
+        
+        kayıtTarihi = DateTime.Now;
+ 
+        
+        }
+
+        // bu constructoru kullanmak için nesne oluştururken direkt değerlerin girilmesi gerekiyor.
+        public Kitap(string kitapadı, string yazaradı, string yazarsoyadı, int sayfasayısı, string basımevi) {
+
+            kitapAdı = kitapadı;
+            yazarAdı = yazaradı;
+            yazarSoyadı = yazarsoyadı;
+            sayfaSayısı = sayfasayısı;
+            basımEvi = basımevi;
+
+            kayıtTarihi = DateTime.Now;
+        
+        
+        }
+
+        public void KitapBilgileriniGöster() {
+
+            Console.WriteLine($"kitabın adı: {kitapAdı} yazaradı: {yazarAdı} yazar soyadı: {yazarSoyadı} sayfasayısı : {sayfaSayısı} basımevi {basımEvi}");
+        
+        }
+
+    }
+}
---
+++ Patikakitapuygulaması/Patikakitapuygulaması/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patikakitapuygulaması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // bu metodda değerleri verip newi de kullanarak nesne oluşturduk. Bunu istediğimiz kadar çoğaltabiliriz.
+            Kitap kitap1 = new Kitap("Ay Bahçeleri", "Steve", "Ericson", 720, "ithaki");
+
+            kitap1.KitapBilgileriniGöster();
+
+
+            Kitap kitap2 = new Kitap();
+
+            kitap2.yazarSoyadı = "Tolkien";
+            kitap2.sayfaSayısı = 1000;
+            kitap2.kitapAdı = "Yüzüklerin Efendisi";
+            kitap2.yazarAdı = "John Ronald Reuel";
+            kitap2.basımEvi = "Metis Yayıncılık";
+
+
+            kitap2.KitapBilgileriniGöster();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
